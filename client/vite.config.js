d6769bc2a24758e81062a2ac97c315eaa7eb4d65
torcanhack <tor409617@gmail.com>
2025-05-09 11:43:20 +0100

    fixed update and add pots features

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'https://finance-9kew.onrender.com/api',  
-    }
-  },
-  build: {
-    outDir: 'dist'
-  }
+  
+ 
 })
