f466c0d3df12ee358254ef45bf07f3cb0e30e331
torcanhack <tor409617@gmail.com>
2025-05-09 03:31:00 +0100

    fixed update and add pots features

--- server/server.js
+++ server/server.js
@@ -13,5 +13,5 @@ app.use(express.urlencoded({extended: true}))
 app.use('/api', recordsRouter)
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
